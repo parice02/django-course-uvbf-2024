b7b4d4f755ee4560f5d52bcf823022afda54234f
Mohamed Zeba <parice02@hotmail.com>
2024-10-02 16:38:21 +0000

    f

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,22 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Layout, Row
 
 
 class PersonForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = "id-exampleForm"
+        self.helper.form_class = "form-inline"
+        self.helper.form_method = "post"
+        self.helper.form_action = "submit_survey"
+        self.helper.label_class = "col-lg-2"
+        self.helper.field_class = "col-lg-8"
+        self.helper.layout = Layout(Row("first_name", "last_name"), "age")
+
+        self.helper.add_input(Submit("submit", "Soumettre"))
+
     first_name = forms.CharField(
         max_length=10,
         label="Prénom",
--- tutouvbf/settings.py
+++ tutouvbf/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_bootstrap5",
     "app",
 ]
 
@@ -123,3 +125,8 @@ STATICFILES_DIRS = [BASE_DIR / "tutouvbf/static"]
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
